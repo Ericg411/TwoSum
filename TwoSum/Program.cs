ecadc3bc72f3924aa1ac00f64e12ff2ba9f2e9b0
Eric Gauthier <ericg411@gmail.com>
2023-01-19 18:39:49 -0600

    Add project files.

---
+++ TwoSum/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace TwoSum
+{
+    internal class Program
+    {
+        //Given an array of integers nums and an integer target, 
+        //return indices of the two numbers such that they add up to target.
+        static void Main(string[] args)
+        {
+            Console.WriteLine("");
+            
+        }
+
+        public int[] TwoSum(int[] nums, int target)
+        {
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int i2 = 0; i2 < nums.Length; i2++)
+                {
+                    if (nums[i] + nums[i2] == target && i != i2)
+                    {
+                        int[] newArr = { i, i2 };
+                        return newArr;
+                    }
+                }
+            }
+            return nums;
+        }
+    }
+}
